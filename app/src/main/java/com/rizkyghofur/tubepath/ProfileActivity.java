b369bb1d73ad8c8b982509f2b5e8def1638a05bc
rizkyghofur <rizky.abdulghofur@gmail.com>
2019-06-06 22:01:34 +0700

    perubahan pada profile activity (penyesuaian layout dasar)

--- app/src/main/java/com/rizkyghofur/tubepath/ProfileActivity.java
+++ app/src/main/java/com/rizkyghofur/tubepath/ProfileActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -51,6 +50,5 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             finish();
             startActivity(new Intent(this, MainMenuActivity.class));
         }
-
     }
 }
